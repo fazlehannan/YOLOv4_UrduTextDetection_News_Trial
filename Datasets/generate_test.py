8894d63ce627b9e02c67561351e613169c1abd6f
Fazle Hanan <fazlehannan@gmail.com>
2020-12-23 00:32:47 +0500

    Change data folder name

--- Datasets/generate_test.py
+++ Datasets/generate_test.py
@@ -1,10 +1,10 @@
 import os
 
 image_files = []
-os.chdir(os.path.join("data", "test"))
+os.chdir(os.path.join("dataurdu", "test"))
 for filename in os.listdir(os.getcwd()):
     if filename.endswith(".jpg"):
-        image_files.append("data/test/" + filename)
+        image_files.append("dataurdu/test/" + filename)
 os.chdir("..")
 with open("test.txt", "w") as outfile:
     for image in image_files:
