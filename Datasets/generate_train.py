6385d1c76a5c692adc0d8dc724ce8a4407653945
Fazle Hanan <fazlehannan@gmail.com>
2020-12-23 00:34:29 +0500

    Change name of data folder

--- Datasets/generate_train.py
+++ Datasets/generate_train.py
@@ -1,10 +1,10 @@
 import os
 
 image_files = []
-os.chdir(os.path.join("data", "obj"))
+os.chdir(os.path.join("dataurdu", "obj"))
 for filename in os.listdir(os.getcwd()):
     if filename.endswith(".jpg"):
-        image_files.append("data/obj/" + filename)
+        image_files.append("dataurdu/obj/" + filename)
 os.chdir("..")
 with open("train.txt", "w") as outfile:
     for image in image_files:
